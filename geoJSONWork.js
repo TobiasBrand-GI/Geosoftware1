4342cde43f065b903a98af6375cc765845978682
TobiasBrand-GI <79696931+TobiasBrand-GI@users.noreply.github.com>
2021-05-07 12:22:35 +0200

    commit

---
+++ geoJSONWork.js
@@ -0,0 +1,26 @@
+'use strict'
+let error;
+/**
+ * Method to call validation check from external library
+ * @param {*} input takes a string or object to check if it is in valid GeoJSON format
+ * @param {*} options string with options for the check
+ * @returns boolean
+ */
+function validationCheck(input, options){
+    // calling validation method from author: https://github.com/mapbox/geojsonhint
+    error=geojsonhint.hint(input, options);
+    if (error.length==0){
+        return true;
+    }else{return false;} 
+}
+
+/**
+ * Function to parse a string to a GeoJSON object
+ * @param {*} data accepts an array of coordinates and parses it into a GeoJSON object
+ * @returns object
+ */
+function parseIntoJSON(data){
+    lineStringData=[{line:data}]
+    // calling parsing method from geojson.min.js, author: https://github.com/caseycesari/GeoJSON.js
+    return GeoJSON.parse(lineStringData, {'LineString': 'line' });
+}
--- main.js
+++ main.js
@@ -1,21 +1,60 @@
 'use strict'
+let oldRoute = 0;
 
 /**
  * The main function called in the webpage
- * Automatically loaded when the page is loaded
+ * Called by Button on Webpage
  */
 function main(){
-    // Starting intersection point algorithm
-    intersectionCheck();
+    let routeString = document.getElementById('routeInput').value;
+    let optionString = "precisionWarning:false";
+    //Check if its a valid GeoJSON
+    if(validationCheck(routeString, optionString)==true){
+        tmpInputMemory=routeString;
+        //Check if it is a new GeoJSON
+        if(oldRoute==0){
+            let tmpJsonObj = JSON.parse(routeString);
+            //Check if JSON is a linestring
+            if(tmpJsonObj.features[0].geometry.type==="LineString"){
+                route=tmpJsonObj.features[0].geometry.coordinates;
+                // Starting intersection point algorithm
+                intersectionCheck();
 
-    // Sorting and rearranging the array containing the results from the intersection algorithm
-    let sortedArray = sortArray(allPathLength);
-    let reversedArray = sortedArray.reverse();
+                // Sorting and rearranging the array containing the results from the intersection algorithm
+                let sortedArray = sortArray(allPathLength);
+                let reversedArray = sortedArray.reverse();
 
-    // Calling method to create the HTML table
-    creatingTable(reversedArray);
+                // Calling method to create the HTML table
+                creatingTable(reversedArray);
+                showUsedGeoJSON();
+            }
+            
+        } else{
+            //Check if JSON is a linestring
+            if(txtAreaReturn.features[0].geometry.type==="LineString"){
+                route=txtAreaReturn.features[0].geometry.coordinates;
+                // Starting intersection point algorithm
+                intersectionCheck();
+
+                // Sorting and rearranging the array containing the results from the intersection algorithm
+                let sortedArray = sortArray(allPathLength);
+                let reversedArray = sortedArray.reverse();
+
+                // Calling method to create the HTML table
+                creatingTable(reversedArray);
+                showUsedGeoJSON();
+            }
+            
+        }
+        
+    }else{
+        // Catch errors
+        errorVisualize();
+    }
+
+   
 }
-window.onload=main;
+
 
 /**
  * sorting an array by using BubbleSort
@@ -33,50 +72,4 @@ function sortArray(sortObject){
         }
     }
     return allPathLength;
-}
-
-/**
- * Taking an array and creating a HTML table object with the array data
- * @param {*} reversedArray 
- */
-function creatingTable(reversedArray){
-    // Table for path coordinates and path length
-    let tablePath = document.createElement('table');
-    tablePath.id = "data";
-    // Creting heading row
-    let rowHeader = tablePath.insertRow();
-    rowHeader.id="headerRow";
-    let header1 = rowHeader.insertCell();
-    header1.textContent = "ID";
-    let header2 = rowHeader.insertCell();
-    header2.textContent = "Startpunkt";
-    let header3 = rowHeader.insertCell();
-    header3.textContent = "Endpunkt";
-    let header4 = rowHeader.insertCell();
-    header4.textContent = "Abschnittslänge in Meter";
-
-    // Creating a row for each entry in the array
-    for (let tr=0; tr<reversedArray.length; tr++){
-        let row = tablePath.insertRow();
-        row.id = "dataRow";
-        // Creating 4 cells for ID, start and end point aswell as the path length
-        let idCell = row.insertCell();
-        idCell.textContent = (tr+1);
-        let startPCell = row.insertCell();
-        startPCell.textContent = reversedArray[tr][0][0]+" °E"+"\n"+reversedArray[tr][0][1]+" °N ";
-        let endPCell = row.insertCell();
-        endPCell.textContent = reversedArray[tr][1][0]+" °E"+"\n"+reversedArray[tr][1][1]+" °N";
-        let lenCell = row.insertCell();
-        lenCell.textContent = reversedArray[tr][2];
-    }
-    document.body.appendChild(tablePath);
-
-    // Table for the summ of all path length
-    let summDistance = getPathSummLength(reversedArray);
-    let tableSumm = document.createElement('table');
-    tableSumm.id="summTab";
-    let row = tableSumm.insertRow();
-    let summCell = row.insertCell();
-        summCell.textContent = "Gesamtlänge der Strecke beträgt: "+summDistance+" Meter, oder umgerechnet etwa: "+(Math.round((summDistance/1000)*100)/100)+" Kilometer!";
-    document.body.appendChild(tableSumm);
 }
