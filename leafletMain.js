878ae34ab50afcd784c7c5d3095b5c94653bb4dc
TobiasBrand-GI <79696931+TobiasBrand-GI@users.noreply.github.com>
2021-05-21 23:51:25 +0200

    Changed Code

--- leafletMain.js
+++ leafletMain.js
@@ -1,17 +1,14 @@
 
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid',{drawControl: true}).setView([52, 9],6);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
-    var geojsonLayer = 
-        $.ajax({
-            url:"src/Route_Uebung4.geojson",
-            dataType: "json",
-            success: console.log("Data successfully loaded!"),
-            error: function (xhr) {
-               alert(xhr.statusText)
-            }
-           })
-           console.log(geojsonLayer);
-    var myLayer = L.geoJSON().addTo(mymap);
-    myLayer.addData(geojsonLayer);
+    var myLayer = L.geoJSON(geoJsonFeature).addTo(mymap);
+    var drawnItems = new L.FeatureGroup();
+    mymap.addLayer(drawnItems);
+    var drawControl = new L.Control.Draw({
+        edit: {
+            featureGroup: drawnItems
+        }
+    });
+    mymap.addControl(drawControl);
